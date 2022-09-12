de6369fa337cf812d9ba25d4f6a366b12c9567f2
Edwin Alexander Cardenas Esteban <eacardenase@unal.edu.co>
2022-09-11 19:20:23 -0500

    Basic routes, authentication and middlewares using express type annotations

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log('Hi there!');
+import express, { Request, Response } from 'express';
+import cookieSession from 'cookie-session';
+
+import loginRoutes from './routes/loginRoutes';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+    cookieSession({
+        keys: ['secret'],
+    })
+);
+
+app.use(loginRoutes);
+
+app.listen(3000, () => {
+    console.log('Listening on port: http://localhost:3000');
+});
---
+++ src/routes/loginRoutes.ts
@@ -0,0 +1,74 @@
+import { Router, Request, Response } from 'express';
+
+import { requireAuth } from '../middlewares/protectedRoute';
+
+interface RequestWithBody extends Request {
+    body: {
+        [key: string]: string | undefined;
+    };
+}
+
+const router = Router();
+
+router.get('/', (req: Request, res: Response) => {
+    if (req.session && req.session.loggedIn) {
+        res.send(`
+            <p>You are logged in</p>
+            <a href="/logout">Logout</a>
+        `);
+    } else {
+        res.send(`
+            <p>You are not logged in</p>
+            <a href="/login">Login</a>
+        `);
+    }
+});
+
+router.get('/logout', (req: Request, res: Response) => {
+    req.session = undefined;
+
+    res.redirect('/');
+});
+
+router.get('/login', (req: Request, res: Response) => {
+    res.send(`
+        <form method="POST">
+            <div>
+                <label>Email</label>
+                <input name="email" type="email"></input>
+            </div>
+            <div>
+                <label>Password</label>
+                <input name="password" type="password"></input>
+            </div>
+            <button>Submit</button>
+        </form>
+    `);
+});
+
+router.post('/login', (req: RequestWithBody, res: Response) => {
+    const { email, password } = req.body;
+
+    if (
+        email &&
+        password &&
+        email === 'eacardenase@gmail.com' &&
+        password === 'qwerty'
+    ) {
+        // marking the user as logged in
+        req.session = {
+            loggedIn: true,
+        };
+
+        // redirect user to the root route
+        res.redirect('/');
+    } else {
+        res.send('Invalid email or password');
+    }
+});
+
+router.get('/protected', requireAuth, (req: Request, res: Response) => {
+    res.send('Welcome to protected route, logged in user');
+});
+
+export default router;
